2154864e5769d03d0a751cb0aeb9d9906eb315aa
Rbaa23 <robertoalvarez.ski@gmail.com>
2026-06-18 16:43:30 -0400

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,25 @@
+module.exports = async (req, res) => {
+  const { endpoint, ...params } = req.query;
+  const key = req.headers['x-api-key'];
+
+  if (!endpoint || !key) {
+    res.status(400).json({ errors: ['missing endpoint or key'] });
+    return;
+  }
+
+  const url = new URL('https://v3.football.api-sports.io' + endpoint);
+  Object.keys(params).forEach((k) => {
+    const v = params[k];
+    if (v !== undefined) url.searchParams.set(k, Array.isArray(v) ? v[0] : v);
+  });
+
+  try {
+    const upstream = await fetch(url.toString(), {
+      headers: { 'x-apisports-key': key }
+    });
+    const data = await upstream.json();
+    res.status(upstream.status).json(data);
+  } catch (e) {
+    res.status(502).json({ errors: ['upstream_failed'] });
+  }
+};
